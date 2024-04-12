f9bd6f0d5ff3dcb20057de695a52e067d682e23d
Joana <joana.rodrigues.simplon@gmail.com>
2024-04-12 16:35:14 +0200

    Prepare profile page & router

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,17 +2,19 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')]
-    public function index(): Response
+    #[Route('/user/{id}', name: 'app_user')]
+    public function index(int $id, UserRepository $userRepository): Response
     {
+        $user = $userRepository->find($id);
         return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+            'user' => $user
         ]);
     }
 }
