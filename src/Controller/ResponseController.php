1a20eb262885db2259e9d0da8a69d6c754a006d2
Joana <joana.rodrigues.simplon@gmail.com>
2024-04-18 17:57:11 +0200

    Response Edit

--- src/Controller/ResponseController.php
+++ src/Controller/ResponseController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Entity\Response as EntityResponse;
 use App\Form\ResponseType;
+use App\Repository\ResponseRepository;
 use App\Repository\ThreadRepository;
+use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\SecurityBundle\Security;
@@ -30,7 +32,6 @@ class ResponseController extends AbstractController
             $entityManager->persist(($response));
             $entityManager->flush();
 
-            
             $this->addFlash('success', 'Le thread a bien été posté.');
 
             return $this->redirectToRoute('app_homepage');
@@ -39,6 +40,35 @@ class ResponseController extends AbstractController
         return $this->render('response/index.html.twig', [
             'formResponse' => $formResponse,
             'id' => $id
+        ]);
+    }
+
+    #[Route('/response/{id}/edit', name: 'app_responseEdit', methods: ['POST', 'GET'])]
+    public function responseEdit(int $id, Request $request, EntityManagerInterface $entityManager, EntityResponse $response): Response
+    {
+        $formResponse = $this->createForm(ResponseType::class, $response);
+        $formResponse->handleRequest($request);
+
+        $idThread = $response->getThread();
+        $idThread = $idThread->getId();
+
+        if ($formResponse->isSubmitted() && $formResponse->isValid()) {
+            $response->setDateUpdate(new DateTime());
+
+            $entityManager->persist($response);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Les modifications ont bien été effectués.');
+
+            return $this->redirectToRoute('app_threadPost', [
+                'id' => $idThread
+            ]);
+        }
+
+        return $this->render('response/index.html.twig', [
+            'id' => $id,
+            'response' => $response,
+            'formResponse' => $formResponse
         ]);
     }
 }
